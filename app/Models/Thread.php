c08d527e0df65570b08e9420692568263b274ff4
Hguerrero0821 <hectorc0822@gmail.com>
2023-09-24 22:33:18 -0500

    agregando filtro de busqueda

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -10,4 +10,19 @@ class Thread extends Model
     use HasFactory;
 
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(category::class);
+    }
+
 }
